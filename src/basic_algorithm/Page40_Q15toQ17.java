d68c12c458da26824e451a53d93e9dadc291bf44
kanghangyul <gang6607@gmail.com>
2021-01-06 23:28:20 +0900

    2021 01 06 23:28 commit

--- src/basic_algorithm/Page40_Q15toQ17.java
+++ src/basic_algorithm/Page40_Q15toQ17.java
@@ -3,10 +3,20 @@ package basic_algorithm;
 import java.util.Scanner;
 
 public class Page40_Q15toQ17 {
-    // 왼쪽 아래가 직각인 이등변 삼각형 출력
-    static void Q15(int a) {
-        for (int i = 0; i <= a; i++) {
-            for (int j = 0; j < i; j++) {
+    static void Q15_2(int a) {
+        int cnt = a;
+
+        for (int i = 0; i < cnt; i++, a--) {
+            for (int j = 0; j < a ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void Q15_1(int a) {
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -21,6 +31,9 @@ public class Page40_Q15toQ17 {
             System.out.println("0보다 큰 수를 입력해주세요.");
             a = scan.nextInt();
         }
-        Q15(a);
+        System.out.println("[왼쪽 아래가 직각인 이등변 삼각형 출력]");
+        Q15_1(a);
+        System.out.println("\n[왼쪽 위가 직각인 이등변 삼각형 출력]");
+        Q15_2(a);
     }
 }
