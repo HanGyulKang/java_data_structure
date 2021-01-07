c04a49aecbfc906e80e9a1ff2ca38c83699e29a5
kanghangyul <gang6607@gmail.com>
2021-01-07 23:14:14 +0900

    2020 01 07 23:14 commit

--- src/basic_algorithm/Page40_Q15toQ17.java
+++ src/basic_algorithm/Page40_Q15toQ17.java
@@ -3,6 +3,67 @@ package basic_algorithm;
 import java.util.Scanner;
 
 public class Page40_Q15toQ17 {
+    static void Q17(int a) {
+        int cnt = 1;
+
+        for (int i = 0; i < a; i++) {
+            for (int j = a - 1; j >= 0; j--) {
+                if(j <= i) {
+                    System.out.print(cnt);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(cnt);
+            }
+            cnt++;
+            System.out.println();
+        }
+    }
+
+    static void Q16(int a) {
+        for (int i = 0; i < a; i++) {
+            for (int j = a - 1; j >= 0; j--) {
+                if(j <= i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void Q15_4(int a) {
+        for (int i = a; i > 0; i--) {
+            for (int j = 1; j <= a; j++) {
+                if(j >= i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void Q15_3(int a) {
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                if(j >= i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     static void Q15_2(int a) {
         int cnt = a;
 
@@ -35,5 +96,13 @@ public class Page40_Q15toQ17 {
         Q15_1(a);
         System.out.println("\n[왼쪽 위가 직각인 이등변 삼각형 출력]");
         Q15_2(a);
+        System.out.println("\n[오른쪽 위가 직각인 이등변 삼각형 출력]");
+        Q15_3(a);
+        System.out.println("\n[오른쪽 아래가 직각인 이등변 삼각형 출력]");
+        Q15_4(a);
+        System.out.println("\n[피라미드]");
+        Q16(a);
+        System.out.println("\n[숫자 피라미드]");
+        Q17(a);
     }
 }
