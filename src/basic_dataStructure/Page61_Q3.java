3fbe4bd64fb4a3f39aa7fa642a14cc356ee0e983
kanghangyul <gang6607@gmail.com>
2021-01-09 21:07:25 +0900

    2020 01 09 21:07 commit

---
+++ src/basic_dataStructure/Page61_Q3.java
@@ -0,0 +1,28 @@
+package basic_dataStructure;
+
+import java.util.Scanner;
+
+public class Page61_Q3 {
+    static int sumOf(int[] a) {
+        int total = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            total += a[i];
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] a = new int[10];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        System.out.println(sumOf(a));
+    }
+
+}
