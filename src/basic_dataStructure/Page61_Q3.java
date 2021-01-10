f76dcb04b9e569089e5cb8fa8d99d14dda34a513
kanghangyul <gang6607@gmail.com>
2021-01-10 23:37:35 +0900

    2020 01 10 23:37 commit

--- src/basic_dataStructure/Page61_Q3.java
+++ src/basic_dataStructure/Page61_Q3.java
@@ -9,13 +9,11 @@ public class Page61_Q3 {
         for (int i = 0; i < a.length; i++) {
             total += a[i];
         }
-
         return total;
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         int[] a = new int[10];
 
         for (int i = 0; i < a.length; i++) {
